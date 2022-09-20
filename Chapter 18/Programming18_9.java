9001d291df9b268d57dc6c8f2fdd1618f46ab0a5
ClintonGDavis <105823004+ClintonGDavis@users.noreply.github.com>
2022-09-20 16:47:45 -0600

    Add files via upload

---
+++ Chapter 18/Programming18_9.java
@@ -0,0 +1,31 @@
+/**
+ * Author: Clint Davis
+ * Date: 9/19/22
+ *
+ * This is my attempt at solving Programming 18-9.
+ */
+
+import java.util.Scanner;
+
+public class Programming18_9 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a string: ");
+        String value = input.nextLine();
+
+        System.out.println("The reverse of the string you entered is: ");
+        reverseDisplay(value);
+    }
+
+    public static void reverseDisplay(String value) {
+        if (value.length() == 1)
+            System.out.print(value);
+        else {
+            System.out.print(value.charAt(value.length() - 1));
+
+            reverseDisplay(value.substring(0, value.length() - 1));
+        }
+    }
+}
