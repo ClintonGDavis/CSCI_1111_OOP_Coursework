4c93630be6f917f3671b13b7d2636cabbf4c5d7b
ClintonGDavis <105823004+ClintonGDavis@users.noreply.github.com>
2022-10-06 14:37:06 -0600

    Add files via upload

---
+++ CSI_1112_Final_Project/View07Controller.java
@@ -0,0 +1,14 @@
+package FinalProject1112;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class View07Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // View07
+    }
+}
