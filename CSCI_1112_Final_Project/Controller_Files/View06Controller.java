26735f41d0da2a5e181d8de9eb11732e5e5deb21
ClintonGDavis <105823004+ClintonGDavis@users.noreply.github.com>
2022-10-06 14:58:38 -0600

    Add files via upload

---
+++ CSCI_1112_Final_Project/Controller_Files/View06Controller.java
@@ -0,0 +1,45 @@
+package FinalProject1112;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class View06Controller extends PageController implements Initializable {
+    @FXML
+    TextField textField1;
+    @FXML
+    TextArea textArea1;
+    @FXML
+    TextField textField2;
+    @FXML
+    TextArea textArea2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        textArea1.setText(balancedLiving.getSafetyStandard());
+        textArea2.setText(balancedLiving.getSafetyScore());
+
+        textField1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                textArea1.setText(textField1.getText());
+                balancedLiving.setSafetyStandard(textField1.getText());
+            }
+        });
+
+        textField2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                textArea2.setText(textField2.getText());
+                balancedLiving.setSafetyScore(textField2.getText());
+            }
+        });
+    }
+}
