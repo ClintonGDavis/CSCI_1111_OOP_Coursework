90ec47bfd7170bb0c14c47f3221e1161989915eb
ClintonGDavis <105823004+ClintonGDavis@users.noreply.github.com>
2022-10-06 14:46:27 -0600

    Add files via upload

---
+++ CSCI_1112_Final_Project/PageController.java
@@ -0,0 +1,77 @@
+package FinalProject1112;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PageController extends BalancedLiving implements Initializable {
+    @FXML
+    AnchorPane apMain;
+
+    BalancedLiving balancedLiving = new BalancedLiving();
+
+    @FXML
+    private void handleButtonAction(ActionEvent event)
+    {
+        try
+        {
+            Pane newLoadedPane;
+            Button tempButton = (Button)event.getSource();
+
+            switch(tempButton.getId())
+            {
+                case "LoveB":
+                    newLoadedPane =  FXMLLoader.load(getClass().getResource("View01.fxml"));
+                    apMain.getChildren().add(newLoadedPane);
+                    break;
+                case "WorkB":
+                    newLoadedPane =  FXMLLoader.load(getClass().getResource("View02.fxml"));
+                    apMain.getChildren().add(newLoadedPane);
+                    break;
+                case "LearningB":
+                    newLoadedPane =  FXMLLoader.load(getClass().getResource("View03.fxml"));
+                    apMain.getChildren().add(newLoadedPane);
+                    break;
+                case "PlayB":
+                    newLoadedPane =  FXMLLoader.load(getClass().getResource("View04.fxml"));
+                    apMain.getChildren().add(newLoadedPane);
+                    break;
+                case "HealthB":
+                    newLoadedPane =  FXMLLoader.load(getClass().getResource("View05.fxml"));
+                    apMain.getChildren().add(newLoadedPane);
+                    break;
+                case "SafetyB":
+                    newLoadedPane =  FXMLLoader.load(getClass().getResource("View06.fxml"));
+                    apMain.getChildren().add(newLoadedPane);
+                    break;
+                case "MainPageB":
+                    newLoadedPane =  FXMLLoader.load(getClass().getResource("View07.fxml"));
+                    apMain.getChildren().add(newLoadedPane);
+                    break;
+                case "SaveB":
+                    newLoadedPane =  FXMLLoader.load(getClass().getResource("View08.fxml"));
+                    apMain.getChildren().add(newLoadedPane);
+                    break;
+            }
+        }
+        catch (IOException ex) {
+            Logger.getLogger(PageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }
+}
---
+++ CSCI_1112_Final_Project/View03Controller.java
@@ -0,0 +1,44 @@
+package FinalProject1112;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class View03Controller extends PageController implements Initializable {
+    @FXML
+    TextField textField1;
+    @FXML
+    TextArea textArea1;
+    @FXML
+    TextField textField2;
+    @FXML
+    TextArea textArea2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        textArea1.setText(balancedLiving.getLearningStandard());
+        textArea2.setText(balancedLiving.getLearningScore());
+
+        textField1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                textArea1.setText(textField1.getText());
+                balancedLiving.setLearningStandard(textField1.getText());
+            }
+        });
+
+        textField2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                textArea2.setText(textField2.getText());
+                balancedLiving.setLearningScore(textField2.getText());
+            }
+        });
+    }
+}
