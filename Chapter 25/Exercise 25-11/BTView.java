187f03c466a415a5529c01cd8b8f12a02e775762
ClintonGDavis <105823004+ClintonGDavis@users.noreply.github.com>
2022-12-02 14:02:04 -0700

    Add files via upload

---
+++ Chapter 25/Exercise 25-11/BTView.java
@@ -0,0 +1,66 @@
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+
+
+public class BTView extends Pane {
+
+    private BST<Integer> tree = new BST<>();
+    private double radius = 15; // Tree node radius
+    private double vGap = 30; // Gap between two levels in a tree
+
+    BTView(BST<Integer> tree) {
+        this.tree = tree;
+        setStatus("Tree is empty");
+    }
+
+    public void setStatus(String msg) {
+        getChildren().add(new Text(20, 20, msg));
+    }
+
+    public void displayTree() {
+        this.getChildren().clear(); // Clear the pane
+        if (tree.getRoot() != null) {
+        	
+            // Display tree recursively
+            displayTree(tree.getRoot(), vGap, getHeight() / 2, getHeight() / 4);
+            
+            //*// ROOT,  X,  Y,  hGAP //*//
+        }
+    }
+
+    /**
+     * Display a subtree rooted at position (x, y)
+     */
+    private void displayTree(BST.TreeNode<Integer> root,
+            double x, double y, double hGap) {
+        if (root.left != null) {
+            // Draw a line to the left node
+            getChildren().add(new Line(x + hGap, y + hGap, x, y));
+            // Draw the left subtree recursively
+            displayTree(root.left, x + hGap, y + hGap, hGap / 2);
+        }
+
+        if (root.right != null) {
+            // Draw a line to the right node
+        	                         //root,    X,       Y,  GAP  
+            getChildren().add(new Line(y - hGap, x + hGap, x, y));
+            // Draw the right subtree recursively
+            displayTree(root.right, y - hGap, x + hGap, hGap / 2);
+        }
+
+        // Display a node
+        Circle circle = new Circle(x, y, radius);
+        circle.setFill(Color.WHITE);
+        circle.setStroke(Color.BLACK);
+        getChildren().addAll(circle,
+                new Text(x - 4, y + 4, root.element + ""));
+        
+        System.out.println(circle.getCenterX() + " " + circle.getCenterY() + "\n");
+        if (circle.getCenterX() == 225 && circle.getCenterY() == 50)
+        System.out.println("Thus anew:\n");
+    }
+}
